7fe42a218dbe6998d844d57299c02c662362e96f
sdias-code <silviodias.ms@gmail.com>
2021-07-27 09:20:59 -0400

    Update ViewModel

--- autenticacaologin/AutenticacaoLogin/AutenticacaoLogin/ViewModels/CadastroUsuarioViewModels.cs
+++ autenticacaologin/AutenticacaoLogin/AutenticacaoLogin/ViewModels/CadastroUsuarioViewModels.cs
@@ -11,13 +11,16 @@ namespace AutenticacaoLogin.ViewModels
         [Required(ErrorMessage = "Informe seu nome")]
         [MaxLength(100, ErrorMessage = "O nome deve ter até 100 caracteres")]
         public string Nome { get; set; }
+
         [Required(ErrorMessage = "Informe seu nome")]
         [MaxLength(50, ErrorMessage = "O login deve ter até 50 caracteres")]
         public string Login { get; set; }
+
         [Required(ErrorMessage = "Informe sua senha")]
         [DataType(DataType.Password)]
         [MinLength(6, ErrorMessage = "A senha deve ter pelo menos 6" + "caracteres")]
         public string Senha { get; set; }
+
         [Required(ErrorMessage = "Confirme sua senha")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirmar senha")]
